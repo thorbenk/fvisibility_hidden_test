718894f7b0bf83156b9b482d38409515c0f86f5a
Thorben Kroeger <dev@thorben.net>
2017-11-20 22:09:43 +0100

    investigate

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,5 +1,11 @@
 #include <testlib.h>
 
+// exported in `testlib`, so we do not have to instantiate here
+extern template class TemplateExportClass<int>;
+
+// does not work, becaues `testlib` does not export the `float` instantiation!
+// extern template class TemplateExportClass<float>;
+
 int main()
 {
     TestLib t;
