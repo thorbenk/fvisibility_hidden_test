ea4b49cb4727df4d4c308a2a4dd60c3ae2cd77f9
Thorben Kroeger <dev@thorben.net>
2017-11-20 22:01:09 +0100

    more ways to instantiate and export templates

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -7,8 +7,13 @@ int main()
     t.test();
     TestLib::test_static();
 
-    TestTemplate<int>();
-    TestTemplate<long>();
+    TemplateExportClass<int>();    // from lib
+    TemplateExportClass<long>();   // from lib
+    TemplateExportClass<float>();  // instantiated here
+
+    TemplateExportExplicitInstantiation<int>(); // from lib
+    TemplateNoExplicitInstantiation<long>();    // from lib
+    TemplateNoExplicitInstantiation<float>();   // instantiated here
 
     return 0;
 }
--- testlib/include/testlib.h
+++ testlib/include/testlib.h
@@ -13,16 +13,28 @@ private:
     int mInt;
 };
 
+// Export each explicit template instantiation
 template<typename T>
-class TESTLIB_EXPORT TestTemplate {
+class TemplateExportClass {
 public:
-    TestTemplate() {
-       std::cout << sizeof(T) << std::endl;
-    }
+    TemplateExportClass() { std::cout << sizeof(T) << std::endl; }
 };
+template class TESTLIB_EXPORT TemplateExportClass<int>;
+template class TESTLIB_EXPORT TemplateExportClass<long>;
 
-template class TestTemplate<int>;
-template class TestTemplate<long>;
+// Export all template instantiations
+template<typename T>
+class TESTLIB_EXPORT TemplateExportExplicitInstantiation {
+public:
+    TemplateExportExplicitInstantiation() { std::cout << sizeof(T) << std::endl; }
+};
+template class TemplateExportExplicitInstantiation<int>;
+template class TemplateExportExplicitInstantiation<long>;
+// not allowed: duplicate explicit instantiation of ‘class TestTemplate2<int>’
+// template class TestTemplate2<int>;
 
-
-
+template<typename T>
+class TESTLIB_EXPORT TemplateNoExplicitInstantiation {
+public:
+    TemplateNoExplicitInstantiation() { std::cout << sizeof(T) << std::endl; }
+};
